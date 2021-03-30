b5ad64e81e37b90f121a14ee9aad3f746d6b4f13
Vishal G. Aswar <vishalaswar25@gmail.com>
2021-03-30 10:49:15 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -84,7 +84,6 @@ prevBtn.addEventListener("click", function () {
 });
 // show random person
 randomBtn.addEventListener("click", function () {
-  console.log("hello");
 
   currentItem = Math.floor(Math.random() * reviews.length);
   showPerson(currentItem);
